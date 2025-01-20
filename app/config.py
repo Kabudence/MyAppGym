1300cdbc0115bcdef7bc995f2be44cdcdd2ffb00
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-01-20 12:27:31 -0500

    feat:added project configs

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:gitano200J%40%40J%40%40@localhost:3306/gamaishx_mystore'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
